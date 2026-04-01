0d22315fb8b0ac20d45095c84f28bb7b1cf678b4
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-04-01 17:36:55 -0300

    changed to Node3D

--- components/contexts/PlayerContext.gd
+++ components/contexts/PlayerContext.gd
@@ -1,5 +1,5 @@
 class_name PlayerContext
-extends Node
+extends Node3D
 
 # controllers
 @export var fpsWalkController: FpsWalkController
@@ -12,13 +12,14 @@ extends Node
 # dados
 @export var player: CharacterBody3D
 @export var pivotRot: Node3D
+@export var standShapeCast3D: ShapeCast3D
 
 
 func _physics_process(_delta: float) -> void:
 	fpsWalkController.run(pivotRot.global_transform)
 	sprintController.run(player.is_on_floor(), Settings.sprintHoldMode, fpsWalkController.state.inputVec, _delta)
 	jumpController.run(player.is_on_floor(), _delta)
-	crouchController.run(Settings.crouchHoldMode, _delta)
+	crouchController.run(Settings.crouchHoldMode, not standShapeCast3D.is_colliding(), _delta)
 	strafeController.run(true, _delta)
 	fpsCameraRotController.run(pivotRot, Settings.cameraSensitivity, Settings.cameraSensitivity, 200.0, 200.0, _delta)
 	pass
