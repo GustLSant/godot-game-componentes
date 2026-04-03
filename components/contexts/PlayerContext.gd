0c6a90aefc74cbe2dc30a5026b8ec2948c6dda0a
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-04-03 01:01:21 -0300

    testes implementacao DiveController

--- components/contexts/PlayerContext.gd
+++ components/contexts/PlayerContext.gd
@@ -2,24 +2,29 @@ class_name PlayerContext
 extends Node3D
 
 # controllers
+@export var gravityController: GravityController
 @export var fpsWalkController: FpsWalkController
 @export var sprintController: SprintController
 @export var jumpController: JumpController
 @export var crouchController: CrouchController
+@export var diveController: DiveController
 @export var strafeController: StrafeController
 @export var fpsCameraRotController: FpsCameraRotController
 
 # dados
 @export var player: CharacterBody3D
+@export var camera: Camera3D
 @export var pivotRot: Node3D
 @export var standShapeCast3D: ShapeCast3D
 
 
 func _physics_process(_delta: float) -> void:
+	gravityController.run(player.is_on_floor(), _delta)
 	fpsWalkController.run(pivotRot.global_transform)
 	sprintController.run(player.is_on_floor(), Settings.sprintHoldMode, fpsWalkController.state.inputVec, _delta)
 	jumpController.run(player.is_on_floor(), _delta)
 	crouchController.run(Settings.crouchHoldMode, not standShapeCast3D.is_colliding(), _delta)
+	diveController.run(fpsWalkController.state.walkVec, player.is_on_floor(), _delta)
 	strafeController.run(true, _delta)
 	fpsCameraRotController.run(pivotRot, Settings.cameraSensitivity, Settings.cameraSensitivity, 200.0, 200.0, _delta)
 	pass
--- components/handlers/PlayerMovementHandler.gd
+++ components/handlers/PlayerMovementHandler.gd
@@ -3,19 +3,69 @@ extends Node
 
 @export var context: PlayerContext
 @export var player: CharacterBody3D
+@export var pivotCrouch: Marker3D
 
 
 func _ready() -> void:
 	context.crouchController.connect("CrouchChanged", handleCrouchChanged)
 	context.sprintController.connect("SprintChanged", handleSprintChanged)
+	
+	
 	pass
 
 
 func _physics_process(_delta: float) -> void:
-	player.velocity = context.fpsWalkController.state.walkVec
-	player.velocity *= context.sprintController.state.speedMultiplier
-	player.velocity *= context.crouchController.state.speedMultiplier
-	player.velocity.y = context.jumpController.state.yMotion
+	player.velocity = Vector3.ZERO
+	
+	pivotCrouch.position.y = context.crouchController.state.heightOffset
+	
+	$"../../StandingCollider".disabled = context.crouchController.state.isCrouched or context.diveController.isDiving
+	$"../../CrouchCollider".disabled = not context.crouchController.state.isCrouched
+	$"../../DiveCollider".disabled = not context.diveController.isDiving
+	#if (context.diveController.state.isDiving): print($"../../DiveCollider".shape.size)
+	#if (context.diveController.state.isDiving): print($"../../DiveCollider".position.y); print('')
+	
+	$"../../DiveCollider".shape.size = context.diveController.currentColliderSize
+	$"../../DiveCollider".position.y = context.diveController.currentColliderHeight
+	
+	player.velocity += context.fpsWalkController.state.walkVec
+	player.velocity.y += context.gravityController.state.motion
+	
+	# PROBLEMA:
+		# TEM QUE TER A VELOCIDADE DO SPRINT NA ADIÇÃO DO DIVE, MAS NAO PODE TER MUDANÇA DE INPUT DE SPRINT DURANTE O DIVE
+	# raiz do problema:
+		# o handler nao tem acesso a quando o SprintController executará
+	# impedimento que causa o problema: 
+		# a ideia era ter varios handlers, um para cada sistema, porem alguns controllers afetarão
+		# mais de um sistema (ex: crouch que mexe na velocidade de movimento e altura da camera) e nao é
+		# recomendado executar um controller mais de uma vez por frame (mesmo? se eh a msm instancia..),
+		# precisaria separar local de injeção de dependencia + execução com o local da utilizacao dos dados
+	
+	# possivel solucao 1:
+		# adicionar uma variavel interna aos controllers para barrar sua execucao
+		# e essa variavel seria controlada pelo handler
+	
+	# possivel solucao 2:
+		# mudar os controllers para ter uma funcao setup onde eles guardariam todas as suas dependencias
+		# essa funcao seria executada pelo context, dai a funcao run seria executada pelos handlers
+		# e como seria uma 're-execução' da mesma instancia, nao teria problema de lógica (tlvz uma pequena perda de performance)
+		
+		# CENARIO QUE PODE DAR ERADO:
+			# so pode alterar o isSprinting enquando nao estiver em dive
+			# no MovementHandler o sprintHandler pode facilmente ser travado
+			# mas quando for re-executar ele para aplicar os efeitos na camera, o isSprinting pode ser alterado (e nao deveria)
+	
+	# possivel solucao 3:
+		# ter um script de resolucao de conflitos e orquestração de quem q vai executar e quando (poderia ser o context?)
+		# e ter um script para usar de fato os valores resolvidos pelos controllers
+	
+	if (not context.diveController.isDiving):
+		player.velocity *= context.sprintController.state.speedMultiplier
+		player.velocity *= context.crouchController.state.speedMultiplier
+		player.velocity.y += context.jumpController.state.motion
+	else:
+		player.velocity += context.diveController.motion
+	
 	player.move_and_slide()
 	pass
 
