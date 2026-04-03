d206ea063c331416afe09e3beece2967d8938dd1
Gustavo LS <93957341+GustLSant@users.noreply.github.com>
2026-04-03 01:00:47 -0300

    removed gravity

--- components/controllers/JumpController.gd
+++ components/controllers/JumpController.gd
@@ -2,27 +2,15 @@ class_name JumpController
 extends Node
 
 class State:
-	var yMotion: float = 0.0
+	var motion: float = 0.0
 
 const JUMP_FORCE: float = 5.0
-const GRAVITY_STRENGTH: float = 20.0
 
 var state: State = State.new()
 
 
 func run(_isPlayerOnFloor: bool, _delta: float) -> void:
-	handleGravity(_isPlayerOnFloor, _delta)
-	handleInput(_isPlayerOnFloor)
-	pass
-
-
-func handleGravity(_isPlayerOnFloor: bool, _delta: float) -> void:
-	if (!_isPlayerOnFloor): state.yMotion -= GRAVITY_STRENGTH * _delta
-	else: state.yMotion = 0.0
-	pass
-
-
-func handleInput(_isPlayerOnFloor: bool) -> void:
-	if (_isPlayerOnFloor and Input.is_action_just_pressed("Jump")):
-		state.yMotion += JUMP_FORCE
+	if (_isPlayerOnFloor):
+		state.motion = 0.0
+		if (Input.is_action_just_pressed("Jump")): state.motion = JUMP_FORCE
 	pass
